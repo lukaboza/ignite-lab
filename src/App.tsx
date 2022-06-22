7c39304f655414ec3666f9ce6b142eea0f117d1d
luka amaral boza <luka.boza@hotmail.com>
2022-06-22 14:05:31 -0300

    live 02 - ignite lab

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,10 @@
-import { gql, useQuery } from "@apollo/client"
-import { useEffect } from "react"
-import { client } from "./lib/apollo"
-
-const GET_LESSONS = gql`
-  query{
-      lessons{
-        id
-        title
-      }
-  }
-`
-
-interface Lesson {    //aqui estamos definindo o tipo de dado que vai ser retornado
-  id: string
-  title: string
-}
+import { Event } from "./pages/Event"
 
 function App() {
-
-  const { data } = useQuery<{ lessons: Lesson[] }>(GET_LESSONS)   //aqui estamos tipando a query com 'Lesson' e puxando o resultado/parametros da query GET_LESSONS
-
-  console.log(data)
-
-  return (
-    <ul>
-    {data?.lessons.map((lesson) => {    
-      return <li key={lesson.id}>{lesson.title}</li>    
-      })
-    }
-    </ul>
+  return(
+    <div>
+      <Event/>
+    </div>
   )
 }
 
