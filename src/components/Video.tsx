557fbaaa33c65b07480bf222c5fb1f92ce9a537d
luka amaral boza <luka.boza@hotmail.com>
2022-06-23 08:46:11 -0300

    commit att. dos arquivos

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -1,7 +1,11 @@
 export function Video() {
     return(
         <div className="flex-1">
-            video
+            <div className="bg-black">
+                <div className="h-full w-full max-w-[1100px] max-h-[60vh] aspect-video">
+                    
+                </div>
+            </div>
         </div>
     )
 }
