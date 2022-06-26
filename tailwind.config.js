1cb9cec96f62b80696328347d571a17552564c74
luka amaral boza <luka.boza@hotmail.com>
2022-06-26 11:55:38 -0300

    final da aula 04 - ignite lab

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 module.exports = {
   content: ["./src/**/*.tsx"],
   theme: {
+      backgroundImage: {
+        blur: 'url(/src/assets/blur-background.png)'
+      },
     extend: {
       fontFamily: {
         sans: 'Roboto, sans-serif'
